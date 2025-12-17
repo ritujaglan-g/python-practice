c6a0698ffdf7b6885b73c906a4ca40e029bbacbc
Ritu Jaglan <156999112+ritujaglan-g@users.noreply.github.com>
2025-12-17 20:42:18 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,10 @@
+user = int(input("put your age how old are you"))
+
+if user >= 18:
+    print(" you are eligible for voting ")
+
+elif user < 18:
+    print(" you are not eligible for voting ")
+
+else:
+    print(" put your age first")
